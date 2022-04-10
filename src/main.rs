37b7202e2c2ff83e6cf09483d4fc7592e6b8c08a
John Reed <87283488+jjreedv@users.noreply.github.com>
2022-04-09 23:46:14 -0700

    looks for contracts in blocks

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,43 @@
-use web3::types::{BlockId, BlockNumber};
+use web3::types::{BlockId, BlockNumber, U64};
 
 #[tokio::main]
 async fn main() -> web3::Result<()> {
-    let transport = web3::transports::http::Http::new(env!("infura_string"))?;
+    let transport = web3::transports::http::Http::new("https://rpc.api.moonbeam.network")?;
     let web3 = web3::Web3::new(transport);
 
-    let number = web3.eth().block_number().await?;
+    let current = web3.eth().block_number().await?;
+
+    let mut number: U64 = U64([190220]);
+
+    while current > number {
+    
+    println!("{}", number);
 
     let blocknumber = BlockNumber::Number(number);
     let block = BlockId::Number(blocknumber);
 
-    let _blockdata = web3.eth().block(block).await?;
+    let blockdata = web3.eth().block(block).await?;
 
-    let pool = web3.txpool().content().await?;
+    match blockdata {
+        Some(block) => {
+            let transactions = block.transactions;
 
-    println!("{:?}", pool);
+            for i in transactions{
+                let receipt = web3.eth().transaction_receipt(i).await?;
 
-
-
+                match receipt {
+                    Some(data) => {
+                        if data.contract_address != None {
+                            println!("{:?}", data.contract_address);
+                        }
+                    }
+                    None => {}
+                }
+            }
+        },
+        None => {}
+    }
+    number = number + 1;
+}
     Ok(())
 }
