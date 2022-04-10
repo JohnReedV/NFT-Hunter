24596d7149e9b75e18087c20471793e9f13faf97
jjreedv <87283488+jjreedv@users.noreply.github.com>
2022-04-10 16:41:11 -0700

    orginize

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,31 @@
-use web3::types::{BlockId, BlockNumber, U64};
+mod contract_address_finder;
+use contract_address_finder::get_contract_address;
+
+use web3::types::{U64, H160};
+
 
 #[tokio::main]
 async fn main() -> web3::Result<()> {
-    let transport = web3::transports::http::Http::new("http://127.0.0.1:8545")?;
+    let transport = web3::transports::http::Http::new("https://rpc.api.moonbeam.network")?;
     let web3 = web3::Web3::new(transport);
 
-    let current = web3.eth().block_number().await?;
-
-    let mut number: U64 = U64([14550000]);
+    let current: U64 = web3.eth().block_number().await?;
+    let mut number: U64 = U64([190220]);
 
     while current > number {
-    
     println!("{}", number);
 
-    let blocknumber = BlockNumber::Number(number);
-    let block = BlockId::Number(blocknumber);
+    let data = get_contract_address(&web3, number).await?;
+    
+    if data.0 == H160([0, 1, 2, 3, 4, 5, 6 ,7 ,8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]) {
+        number = number + U64([1]);
+        continue; }
+    
+    let contract_address = data.0;
+    println!("{:?}", contract_address);
 
-    let blockdata = web3.eth().block(block).await?;
-
-    match blockdata {
-        Some(data) => {
-            let transactions = data.transactions;
-
-            for i in transactions {
-                let receipt = web3.eth().transaction_receipt(i).await?;
-
-                match receipt {
-                    Some(data) => {
-                        if data.contract_address != None {
-                            println!("{:?}", data.contract_address.unwrap());
-                        }
-                    }
-                    None => {}
-                }
-            }
-        }
-        None => {}
+    number = number + U64([1]);
     }
-
-    number = number + 1;
-}
+    
     Ok(())
 }
