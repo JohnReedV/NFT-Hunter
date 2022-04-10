3afbbecde96c7325b294c729b00559593576c447
jjreedv <87283488+jjreedv@users.noreply.github.com>
2022-04-10 03:56:05 -0700

    safer

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ use web3::types::{BlockId, BlockNumber, U64};
 
 #[tokio::main]
 async fn main() -> web3::Result<()> {
-    let transport = web3::transports::http::Http::new("https://rpc.api.moonbeam.network")?;
+    let transport = web3::transports::http::Http::new("http://127.0.0.1:8545")?;
     let web3 = web3::Web3::new(transport);
 
     let current = web3.eth().block_number().await?;
 
-    let mut number: U64 = U64([190220]);
+    let mut number: U64 = U64([14550000]);
 
     while current > number {
     
@@ -16,15 +16,28 @@ async fn main() -> web3::Result<()> {
     let blocknumber = BlockNumber::Number(number);
     let block = BlockId::Number(blocknumber);
 
-    let blockdata = web3.eth().block(block).await?.unwrap();
-    let transactions = blockdata.transactions;
+    let blockdata = web3.eth().block(block).await?;
 
-    for i in transactions {
-        let receipt = web3.eth().transaction_receipt(i).await?.unwrap();
-        if receipt.contract_address != None {
-            println!("{:?}", receipt.contract_address.unwrap());
+    match blockdata {
+        Some(data) => {
+            let transactions = data.transactions;
+
+            for i in transactions {
+                let receipt = web3.eth().transaction_receipt(i).await?;
+
+                match receipt {
+                    Some(data) => {
+                        if data.contract_address != None {
+                            println!("{:?}", data.contract_address.unwrap());
+                        }
+                    }
+                    None => {}
+                }
+            }
         }
+        None => {}
     }
+
     number = number + 1;
 }
     Ok(())
