4d20d163ce49723402cf84f0c4b96c95491121eb
John Reed <87283488+jjreedv@users.noreply.github.com>
2022-07-09 12:55:13 -0700

    refactor

--- src/check_contract.rs
+++ src/check_contract.rs
@@ -1,45 +1,45 @@
+#![allow(unused_variables)]
 use web3::{api::Web3, contract::{Contract, Options}, transports::http::Http, types::{Address, H160, U256}};
 use hex_literal::hex;
 use std::fs::File;
 
-pub async fn get_contract(web3: &Web3<Http>, contract_address: H160) {
-    let is721: bool;
-    let is1155: bool;
+pub async fn check_contract(web3: &Web3<Http>, contract_address: H160) {
+    let mut is721: bool = false;
+    let mut is1155: bool = false;
 
     let data721: Option<bool> = check721(web3, contract_address).await.ok();
     match data721 {
-        Some(result) => { if result == true {is721 = true} else {is721 = false} }
+        Some(result) => { if result == true {is721 = true} }
         None => {}
     }
 
-    let data1155: Option<bool> = check721(web3, contract_address).await.ok();
+    let data1155: Option<bool> = check1155(web3, contract_address).await.ok();
     match data1155 {
-        Some(result) => { if result == true {is1155 = true} else {is1155 = false} }
+        Some(result) => { if result == true {is1155 = true} }
         None => {}
     }
-
 }
 
 pub async fn check721(web3: &Web3<Http>, contract_address: H160) -> web3::contract::Result<bool> {
-    let tokenID: u64 = 1;
-    let mut file = File::open("./ABIs/721ABI.json").unwrap();
+    let token_id: u64 = 1;
+    let file = File::open("./ABIs/721ABI.json").unwrap();
     let abi = web3::ethabi::Contract::load(file).unwrap();
     let contract = Contract::new(web3.eth(), contract_address, abi);
 
-    let result = contract.query("ownerOf", tokenID, None, Options::default(), None);
+    let result = contract.query("ownerOf", token_id, None, Options::default(), None);
     let owner: Address = result.await?;
 
     Ok(true)
 }
 
 pub async fn check1155(web3: &Web3<Http>, contract_address: H160) -> web3::contract::Result<bool> {
-    let tokenID: u64 = 1;
+    let token_id: u64 = 1;
     let my_address: H160 = hex!("A07876136c3A3141Dc2C25071330D3B08225e043").into();
-    let mut file = File::open("./ABIs/1155ABI.json").unwrap();
+    let file = File::open("./ABIs/1155ABI.json").unwrap();
     let abi = web3::ethabi::Contract::load(file).unwrap();
     let contract = Contract::new(web3.eth(), contract_address, abi);
 
-    let result = contract.query("balanceOf", (my_address, tokenID), None, Options::default(), None);
+    let result = contract.query("balanceOf", (my_address, token_id), None, Options::default(), None);
     let amount: U256 = result.await?;
 
     Ok(true)
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+#![allow(unused_variables)]
 mod contract_address_finder;
 use contract_address_finder::get_contract_address;
-mod contract;
-use contract::get_contract;
+mod check_contract;
+use check_contract::check_contract;
 use web3::types::{H160, U64};
 
 #[tokio::main]
@@ -9,25 +10,25 @@ async fn main() -> web3::Result<()> {
     let transport = web3::transports::http::Http::new("https://rpc.api.moonbeam.network")?;
     let web3 = web3::Web3::new(transport);
 
-    let mut currentBlock: U64 = web3.eth().block_number().await?;
-    let mut blockNumber: U64 = U64([252617]);
+    let mut current_block: U64 = web3.eth().block_number().await?;
+    let mut block_number: U64 = U64([1400000]);
 
     loop {
-        println!("{}", blockNumber);
+        println!("{}", block_number);
 
-        let data = get_contract_address(&web3, blockNumber).await?;
+        let data = get_contract_address(&web3, block_number).await?;
         if data.0 == H160::default() {
-            blockNumber = blockNumber + U64([1]);
+            block_number = block_number + U64([1]);
             continue;
         }
 
         let contract_address = data.0;
         println!("{:?}", contract_address);
 
-        let contract = get_contract(&web3, contract_address).await;
+        let contract = check_contract(&web3, contract_address).await;
 
-        blockNumber = blockNumber + U64([1]);
-        currentBlock = web3.eth().block_number().await?;
+        block_number = block_number + U64([1]);
+        current_block = web3.eth().block_number().await?;
     }
 
     Ok(())
